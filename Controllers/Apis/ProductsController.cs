0144f9827520d87804154dbbfcc19071e3e1c98b
Mohamed <alymohamad91@gmail.com>
2022-03-25 19:55:51 +0200

    add-table-features

--- Controllers/Apis/ProductsController.cs
+++ Controllers/Apis/ProductsController.cs
@@ -4,7 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Linq.Dynamic.Core;
+
 using E_commerce_web.Data;
+using E_commerce_web.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace E_commerce_web.Controllers.Apis
@@ -20,12 +23,39 @@ namespace E_commerce_web.Controllers.Apis
             _context = context;
         }
 
-        [HttpGet]
+        [HttpPost]
         public async Task<IActionResult> GetProducts()
         {
-            var products =await _context.Products.ToListAsync();
 
-            return Ok(products);
+            // Pagination
+            var length =int.Parse(Request.Form["length"]);
+
+            var start = int.Parse(Request.Form["start"]);
+
+
+            // Search
+            var searchValue = Request.Form["search[value]"];
+
+            var products =  _context.Products.Where(p=> 
+                string.IsNullOrEmpty(searchValue) || p.Name.Contains(searchValue) || p.Content.Contains(searchValue) || p.Rate.ToString().Contains(searchValue)|| p.Price.ToString().Contains(searchValue));
+
+
+            // Sort
+            var sortColumn = Request.Form[string.Concat("columns[", Request.Form["order[0][column]"], "][name]")];
+            var sortDirection = Request.Form["order[0][dir]"];
+
+            if (!(string.IsNullOrEmpty(sortColumn) && string.IsNullOrEmpty(sortDirection)))
+               products = products.OrderBy(string.Concat(sortColumn, " ", sortDirection));
+
+
+
+            var data =await products.Skip(start).Take(length).ToListAsync();
+
+            var count = products.Count();
+
+            var jsonData = new { recordsFiltered = count, count, data };
+
+            return Ok(jsonData);
         }
     }
 }

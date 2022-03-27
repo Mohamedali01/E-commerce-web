95f82d39a2968372e2c90345e19f55a87c888670
Mohamed <alymohamad91@gmail.com>
2022-03-27 23:04:13 +0200

    upload-image-function

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce_web.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace E_commerce_web.Controllers
 {
+    [Authorize(Roles = RoleName.Admin)]
     public class ProductsController : Controller
     {
         public IActionResult Index()
---
+++ testController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_commerce_web
+{
+    public class testController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            // Get the image from the form
+            var image = Request.Form.Files.FirstOrDefault();
+
+
+            // Open connection
+            var dataStream = new MemoryStream();
+
+           await image.CopyToAsync(dataStream);
+
+           // image as byte[]
+           var imageAsBytes = dataStream.ToArray();
+
+           return Ok();
+        }
+    }
+}

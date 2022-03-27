634ee03f69fd6e288d325290c520108870f782e9
Mohamed <alymohamad91@gmail.com>
2022-03-27 23:12:02 +0200

    upload-image-validation

--- testController.cs
+++ testController.cs
@@ -13,7 +13,39 @@ namespace E_commerce_web
         {
 
             // Get the image from the form
-            var image = Request.Form.Files.FirstOrDefault();
+            var files = Request.Form.Files;
+
+            var image = files.FirstOrDefault();
+            //Ensure that we have a form 
+            if (!files.Any())
+            {
+                ModelState.AddModelError("Poster", "Please Add movie image");
+               
+                // Should return view instead
+                return Ok();
+            }
+
+
+            var extensions = new List<string> { ".jpg", ".png" };
+
+
+            if (!extensions.Contains(Path.GetExtension(image.FileName).ToLower()))
+            {
+                ModelState.AddModelError("Poster", "Image extension is not acceptable");
+
+                // Should return view instead
+                return Ok();
+            }
+
+
+            if (image.Length > 1048576)
+            {
+                ModelState.AddModelError("Poster", "Image size can't be bigger than 1 MB");
+                // Should return view instead
+                return Ok();
+            }
+
+
 
 
             // Open connection
@@ -21,10 +53,12 @@ namespace E_commerce_web
 
            await image.CopyToAsync(dataStream);
 
+
            // image as byte[]
            var imageAsBytes = dataStream.ToArray();
 
            return Ok();
+            
         }
     }
 }

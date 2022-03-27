ce24064c2cd5de5551bc65df83cf464eb56e6901
eng_mahmoud99 <eng.m.ahmed99@gmail.com>
2022-03-28 00:07:16 +0200

    CategoryImage

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using E_commerce_web.Data;
+using E_commerce_web.Models;
+using E_commerce_web.ViewModels;
+using Microsoft.AspNetCore.Hosting;
 
 namespace E_commerce_web.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public CategoryController(ApplicationDbContext context , IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            webHostEnvironment = hostEnvironment;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +30,45 @@ namespace E_commerce_web.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Store(CategoryViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View("Create");
+
+
+            string uniqueFileName = ProcessUploadedFile(model);
+            var row = new Category
+            {
+                Name = model.Name,
+                Image = uniqueFileName
+            };
+
+            _context.Add(row);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        private string ProcessUploadedFile(CategoryViewModel model)
+        {
+            string uniqueFileName = null;
+
+            if (model.Image != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "Uploads");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + model.Image.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                     model.Image.CopyTo(fileStream);
+                }
+            }
+
+            return uniqueFileName;
         }
 
     }
---
+++ ViewModels/CategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace E_commerce_web.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        public string Name { get; set; }
+
+        public IFormFile Image { get; set; }
+
+        public int? CategoryId { get; set; }
+    }
+}
